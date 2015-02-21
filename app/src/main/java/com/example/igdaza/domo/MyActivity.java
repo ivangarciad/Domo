c3c0563b0e1f37d583b7b648e0e324811d31a980
Ivan Garcia <ivan.garcia.daza@gmail.com>
2015-02-21 18:12:46 +0100

    Action bar setting has been implemented, Server IP can be introduce into the application

--- app/src/main/java/com/example/igdaza/domo/MyActivity.java
+++ app/src/main/java/com/example/igdaza/domo/MyActivity.java
@@ -13,7 +13,11 @@ import java.net.*;
 import java.io.*;
 import java.lang.Thread;
 
-import org.json.*;
+import android.content.Context;
+
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class MyActivity extends Activity {
     //private TextView output;
@@ -22,14 +26,38 @@ public class MyActivity extends Activity {
     private BufferedReader reader;
 
     private Thread socketThread;
+    private String filename = "ip_host.txt";
+    private String temp="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
+        //String string = "192.168.0.100";
+
+        try {
+            //FileOutputStream outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            //outputStream.write(string.getBytes());
+            //outputStream.close();
+
+            FileInputStream fin = openFileInput(filename);
+            int c;
+            while( (c = fin.read()) != -1){
+                temp = temp + Character.toString((char)c);
+            }
+            Log.d("FILE-READ", temp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         Runnable runnable = new Runnable(){
             public void run() {
                 try {
-                    client = new Socket("192.168.0.100", 5000);
+                    String ip_name;
+                    Log.d("FILE-CONTENT_IN THREAD", temp);
+                    client = new Socket(temp, 5000);
                     printwriter = new PrintWriter(client.getOutputStream());
                     reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
@@ -93,9 +121,9 @@ public class MyActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        Log.d("NETWORK-RECEIVE", "optiansmenu");
+        Log.d("CreateOptionsMenu", "Menu");
         getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -103,9 +131,13 @@ public class MyActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        Log.d("NETWORK-RECEIVE", "optiansitem");
+        Log.d("OptionsItemSelected", "MenuAction");
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            // Setting activity must be called.
+            Intent intent = new Intent(MyActivity.this, MyActivity3.class);
+            intent.putExtra("file_name", filename);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
